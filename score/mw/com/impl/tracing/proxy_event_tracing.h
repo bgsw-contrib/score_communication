c98ec52fdff380df0110384b318fb888330f0391
Rutuja-Patil-Bosch <tzi1kor@bosch.com>
2026-07-24 14:29:13 +0530

    Improvement: Add tracing for subscription-states

--- score/mw/com/impl/tracing/proxy_event_tracing.h
+++ score/mw/com/impl/tracing/proxy_event_tracing.h
@@ -53,7 +53,17 @@ void TraceCallGetNewSamplesCallback(ProxyEventTracingData& proxy_event_tracing_d
                                     const ProxyEventBindingBase& proxy_event_binding_base,
                                     ITracingRuntime::TracePointDataId trace_point_data_id);
 void TraceCallReceiveHandler(ProxyEventTracingData& proxy_event_tracing_data,
-                             const ProxyEventBindingBase& proxy_event_binding_base);
+                             const ProxyEventBindingBase& proxy_event_binding_base) noexcept;
+void TraceSubscriptionStateChanged(ProxyEventTracingData& proxy_event_tracing_data,
+                                   const ProxyEventBindingBase& proxy_event_binding_base,
+                                   SubscriptionState new_state) noexcept;
+void TraceSetSubscriptionStateChangeHandler(ProxyEventTracingData& proxy_event_tracing_data,
+                                            const ProxyEventBindingBase& proxy_event_binding_base) noexcept;
+void TraceUnsetSubscriptionStateChangeHandler(ProxyEventTracingData& proxy_event_tracing_data,
+                                              const ProxyEventBindingBase& proxy_event_binding_base) noexcept;
+void TraceCallSubscriptionStateChangeHandler(ProxyEventTracingData& proxy_event_tracing_data,
+                                             const ProxyEventBindingBase& proxy_event_binding_base,
+                                             SubscriptionState new_state) noexcept;
 
 score::cpp::callback<void(void), 128U> CreateTracingReceiveHandler(
     ProxyEventTracingData& proxy_event_tracing_data,
